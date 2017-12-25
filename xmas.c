1a22b1587d4e5e49e5f6b4af777e89bda48fa0f1
Alexander Nicholi <me@nicholatian.com>
2017-12-25 04:10:07 -0500

    Initial commit

---
+++ xmas.c
@@ -0,0 +1,10 @@
+/** -*- coding: utf-8; mode: C; indent-tabs-mode: nil; c-basic-offset: 4 -*- */
+
+#include <stdio.h>
+
+int main( int ac, char* av[] )
+{
+    printf( "Merry Christmas!\n" );
+
+    return 0;
+}
